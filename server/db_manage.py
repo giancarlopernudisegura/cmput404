df5ce51d53fb0c5e257a45a43728a6d296f16118
Pegmode <dchu@ualberta.ca>
2022-02-08 18:39:37 +0000

    added skeleton db migration

---
+++ server/db_manage.py
@@ -0,0 +1,21 @@
+#local migration
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager
+from server import app, db
+import os
+
+
+
+app.config.from_obkect(os.getenv("DATABASE_URL"))
+
+migrate = Migrate(app, db)
+
+# manager = Manager(app)
+# manager.add_command("db", MigrateCommand)
+
+
+if __name__ == '__main__':
+    pass
+    #manager.run()
