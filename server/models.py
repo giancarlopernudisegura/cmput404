21bc866626d2f3e5b6da241125816efdff9fc96f
Pegmode <dchu@ualberta.ca>
2022-02-08 17:36:14 +0000

    Added missing content after merge

--- server/models.py
+++ server/models.py
@@ -3,3 +3,13 @@ from sqlalchemy.dialects.postgresql import JSON
 
 
 #Models go here
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    githubId = db.Column(db.String())
+    profileImageId = db.Column(db.String())
+
+    def __init__(self, githubId, profileImageId):
+        self.githubId = githubId
+        self.profileImageId = profileImageId
+    def __repr__(self):
+        return f"<id {self.id}>"
