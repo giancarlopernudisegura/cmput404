4b1e8ddfff72e09a10a533c9a54d7c7bed4bac57
Pegmode <dchu@ualberta.ca>
2022-02-09 11:05:21 +0000

    updated models

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from server import db
+from enums import ContentType, RequestStatus
 
 
 #Models go here
@@ -23,20 +24,21 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.ForeignKey('author.id'))
     title = db.Column(db.String())
-    category = db.Column(db.String())#investigate enum?
+    category = db.Column(db.String())
     private = db.Column(db.Boolean())
     content = db.Column(db.String())
-    contentType = db.Column(db.String())#enum?
-    #timestamp = db.Column(db.Integer)# CHECK TYPE
-    likes = db.Column(db.ForeignKey('author.id'))
+    contentType = db.Column(db.Enum(ContentType))
+    timestamp = db.Column(db.DateTime())
+    likes = db.Column(db.ForeignKey('author.id'))#NEEDS TO CHANGE TO LIST
 
-    def __init__(self, author, title, category, private, content, contentType):
+    def __init__(self, author, title, category, private, content, contentType, timestamp):
         self.author = author
         self.title = title
         self.category = category
         self.private = private
         self.content = content
         self.contentType = contentType
+        self.timestamp = timestamp
 
     def __repr__(self):
         return f"<id {self.id}>"
@@ -45,16 +47,17 @@ class Commment(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True)
     author = db.Column(db.ForeignKey('author.id'))
-    content = db.Column(db.String())#ENUM?
-    contentType = db.Column(db.String())#ENUM?
-    #timestamp = db.Column(db.Integer)# CHECK TYPE
-    likes = db.Column(db.ForeignKey('author.id'))
+    content = db.Column(db.String())
+    contentType = db.Column(db.Enum(ContentType))
+    timestamp = db.Column(db.DateTime())
+    likes = db.Column(db.ForeignKey('author.id'))#NEEDS TO CHANGE TO LIST
 
-    def __init__(self, author, title, contentType, content):
+    def __init__(self, author, title, contentType, content, timestamp):
         self.author = author
         self.title = title
         self.contentType = contentType
         self.content = content
+        self.timestamp = timestamp
 
     def __repr__(self):
         return f"<id {self.id}>"
@@ -62,10 +65,10 @@ class Commment(db.Model):
 class Requests(db.Model):
     __tablename__ = 'requests'
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String())#ENUM?
+    status = db.Column(db.Enum(RequestStatus))
     initiated = db.Column(db.ForeignKey('author.id'))
     to = db.Column(db.ForeignKey('author.id'))
-    #timestamp = db.Column(db.Integer)# CHECK TYPE
+    timestamp = db.Column(db.DateTime())#DO WE NEED THIS?
 
     def __init__(self):
         pass #SETUP DEFAULTS
