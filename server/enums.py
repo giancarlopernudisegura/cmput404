a90930106b5de32c7d1fd23dfb3c7d697352d1b4
Pegmode <dchu@ualberta.ca>
2022-02-10 22:56:35 +0000

    updated enums

--- server/enums.py
+++ server/enums.py
@@ -3,19 +3,19 @@ import enum
 
 #starter for db models. feel reorganize or remove once/if internal classses for these are implemented
 
-class ContentType(enum.Enum):
-    markdown = "text/markdown"
-    plain = "text/plain"
-    #application = "application/base64"
-    png = "image/png"
-    jpeg = "image/jpeg"
 
 class ImageContentType(enum.Enum):
-    png = "image/png"
-    jpeg = "image/jpeg"
+    png = "image/png;base64"
+    jpeg = "image/jpeg;base64"
 
 class TextContentType(enum.Enum):
     markdown = "text/markdown"
     plain = "text/plain"
     #application = "application/base64"
 
+class ContentType(enum.Enum):
+    markdown = TextContentType.markdown
+    plain = TextContentType.plain
+    #application = "application/base64"
+    png = ImageContentType.png
+    jpeg = ImageContentType.jpeg
