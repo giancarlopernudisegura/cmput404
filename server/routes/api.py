fe9d31de1558409f68248f7d964bd6f731bf2a5f
Pegmode <dchu@ualberta.ca>
2022-02-13 04:12:20 +0000

    return codes

--- server/routes/api.py
+++ server/routes/api.py
@@ -29,7 +29,7 @@ def single_author(author_id):
         #request.form.get('displayName')
 
 @bp.route("authors/<author_id>/posts/", methods=['GET', 'POST'])
-def create_post(author_id):
+def post(author_id):
     if request.method == "GET":
         pass
     elif request.method == "POST":
@@ -42,10 +42,13 @@ def create_post(author_id):
             contentType= ContentType(request.form.get("contentType"))
         except:
             pass#return bad
+            return "bad content type", 400
 
         if not request.form.get("visibility") in post_visibility_map:
             pass#return bad
+            return "bad visibility", 400
         private = post_visibility_map[request.form.get("visibility")]
         post = Post(author, title, category, content, contentType)
         db.session.add(post)
         db.session.commit()
+        return "post created", 201
