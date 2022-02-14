3209940e29368e43bd0f329f690ff00edf51494d
Della <humanita.1999@gmail.com>
2022-02-14 15:33:28 -0700

    removed helloworld

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,8 @@ import { Provider } from 'unistore/preact'
 
 import store from './store/store'
 
-import HelloWorld from './views/hello_world'
-
 import './css/main.css'
+
 import ExplorePage from './views/ExplorePage'
 import Header from './components/Header'
 
