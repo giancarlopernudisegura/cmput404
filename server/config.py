05cdde3aaf343fa9a932335fe73b337517dd9713
giancarlopernudisegura <pernudig@gmail.com>
2022-02-13 21:18:20 -0700

    assign replace function result to DATABASE_URL variable

--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 
 class BaseConfig:
