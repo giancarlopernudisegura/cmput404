6c05ca6d263e41a6c8bfd7c854b8e16f2244f912
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2022-02-21 03:44:47 -0700

    add authentication for certain post API endpoints

--- server/constants/res_msg.py
+++ server/constants/res_msg.py
@@ -1,7 +1,13 @@
 # /login endpoint
+from http.client import CONFLICT
+
+
 SUCCESS_USER_CREATED = "User was successfully created"
 SUCCESS_VERIFY_USER = "User credentials are valid"
 
 GENERAL_ERROR = "There was an error "
 
 TOKEN_MISSING = "Token was not included in request."
+
+NO_PERMISSION = "You do not have permission to perform this action."
+CREATE_CONFLICT = "The resource you are trying to create is already exists."
--- server/routes/api.py
+++ server/routes/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, make_response, request, Response
 from server.constants import res_msg
-from flask_login import login_user, login_required
+from flask_login import login_user, login_required, current_user
 from server.exts import db
 from server.models import Author, Post
 from server.enums import ContentType
@@ -9,16 +9,14 @@ from http import HTTPStatus as httpStatus
 import server.utils.api_support as utils
 from typing import Dict, Tuple
 
-bp = Blueprint('api', __name__)
+bp = Blueprint("api", __name__)
 
-post_visibility_map = {
-    "PUBLIC": False,
-    "FRIENDS": True
-}
+post_visibility_map = {"PUBLIC": False, "FRIENDS": True}
 
 
-def pagination(arguments: Dict[str, str], default_page_size: int = 10,
-               default_page_number: int = 1) -> Tuple[int, int]:
+def pagination(
+    arguments: Dict[str, str], default_page_size: int = 10, default_page_number: int = 1
+) -> Tuple[int, int]:
     """Parse the pagination information from the url arguments.
 
     Arguments:
@@ -28,13 +26,12 @@ def pagination(arguments: Dict[str, str], default_page_size: int = 10,
 
     Returns:
         A tuple of (page, size) to use for pagination."""
-    size = int(arguments.get('size', str(default_page_size)), base=10)
-    page_number = int(arguments.get(
-        'page_number', str(default_page_number)), base=10)
+    size = int(arguments.get("size", str(default_page_size)), base=10)
+    page_number = int(arguments.get("page_number", str(default_page_number)), base=10)
     return page_number, size
 
 
-@bp.route('/authors', methods=['GET'])
+@bp.route("/authors", methods=["GET"])
 def multiple_authors() -> Response:
     """Get multiple author.
 
@@ -46,14 +43,20 @@ def multiple_authors() -> Response:
     """
     page, size = pagination(request.args)
     authors = Author.query.paginate(page=page, per_page=size).items
-    return make_response(jsonify(
-        type='authors',
-        items=[a.json() for a in authors],
-        page=page,
-        size=len(authors))), httpStatus.OK
+    return (
+        make_response(
+            jsonify(
+                type="authors",
+                items=[a.json() for a in authors],
+                page=page,
+                size=len(authors),
+            )
+        ),
+        httpStatus.OK,
+    )
 
 
-@bp.route("/authors/<int:author_id>", methods=['GET', 'POST'])
+@bp.route("/authors/<int:author_id>", methods=["GET", "POST"])
 def single_author(author_id: int) -> Response:
     """Get or update a single author.
 
@@ -71,25 +74,38 @@ def single_author(author_id: int) -> Response:
         pass
 
 
-@bp.route("/authors/<int:author_id>/posts/<int:post_id>", methods=['GET'])
+@bp.route("/authors/<int:author_id>/posts/<int:post_id>", methods=["GET"])
 def get_post(author_id: int, post_id: int) -> Response:
     post = Post.query.filter_by(id=post_id).first_or_404()
     return make_response(jsonify(post.json())), httpStatus.OK
 
 
-@bp.route("/authors/<int:author_id>/posts/", methods=['GET', 'POST'])
+@bp.route("/authors/<int:author_id>/posts/", methods=["GET", "POST"])
 def post(author_id: int) -> Response:
     if request.method == "GET":
         page, size = pagination(request.args)
-        posts = Post.query.filter_by(
-            author=author_id, private=False).paginate(
-                page=page, per_page=size).items
-        return make_response(jsonify(
-            type='posts',
-            items=[post.json() for post in posts],
-            size=len(posts),
-            page=page)), httpStatus.OK
+        posts = (
+            Post.query.filter_by(author=author_id, private=False)
+            .paginate(page=page, per_page=size)
+            .items
+        )
+        return (
+            make_response(
+                jsonify(
+                    type="posts",
+                    items=[post.json() for post in posts],
+                    size=len(posts),
+                    page=page,
+                )
+            ),
+            httpStatus.OK,
+        )
     elif request.method == "POST":
+        if current_user.is_anonymous or current_user.id != author_id:
+            return (
+                make_response(jsonify(error=res_msg.NO_PERMISSION)),
+                httpStatus.UNAUTHORIZED,
+            )
         author = author_id
         title = request.form.get("title")
         category = request.form.get("category")
@@ -105,43 +121,50 @@ def post(author_id: int) -> Response:
             # bad content type
             return Response(status=httpStatus.BAD_REQUEST)
 
-        if not request.form.get("visibility") in post_visibility_map:
+        if (
+            not (visibility := request.form.get("visibility").upper())
+            in post_visibility_map
+        ):
             # bad visibility type or no visibility given
             return Response(status=httpStatus.BAD_REQUEST)
-        private = post_visibility_map[request.form.get("visibility")]
+        private = post_visibility_map[visibility.upper()]
 
-        post = Post(author, title, category, content,
-                    contentType, private, unlisted)
+        post = Post(author, title, category, content, contentType, private, unlisted)
         db.session.add(post)
         db.session.commit()
         return Response(status=httpStatus.OK)
 
 
-@bp.route("/authors/<int:author_id>/posts/<int:post_id>", methods=['POST', 'PUT', 'DELETE'])
+@bp.route(
+    "/authors/<int:author_id>/posts/<int:post_id>", methods=["POST", "PUT", "DELETE"]
+)
+@login_required
 def specific_post(author_id: int, post_id: int) -> Response:
-    """Modify create or delete a specific post."""
-    if request.method in ("PUT", "POST", "DELETE"):
-        # TODO: replace with check if logged in user is the author of the post
-        if False:
-            return Response(status=httpStatus.UNAUTHORIZED)
-        if request.method != "PUT":
-            post = Post.query.filter_by(id=post_id).first_or_404()
-        if request.method != "DELETE":
-            author = author_id
-            title = request.form.get("title")
-            category = request.form.get("category")
-            content = request.form.get("content")
-            unlisted = request.form.get("unlisted") or False
-            try:
-                contentType = ContentType(request.form.get("contentType"))
-            except ValueError:
-                return Response(status=httpStatus.BAD_REQUEST)
-            if not request.form.get("visibility") in post_visibility_map:
-                return Response(status=httpStatus.BAD_REQUEST)
-            private = post_visibility_map[request.form.get("visibility")]
-    if request.method == "GET":
-        return make_response(jsonify(post.json())), httpStatus.OK
-    elif request.method == "POST":
+    """Modify, create or delete a specific post."""
+    if current_user.id != author_id:
+        return (
+            make_response(jsonify(error=res_msg.NO_PERMISSION)),
+            httpStatus.UNAUTHORIZED,
+        )
+    if request.method != "PUT":
+        post = Post.query.filter_by(id=post_id).first_or_404()
+    if request.method != "DELETE":
+        author = author_id
+        title = request.form.get("title")
+        category = request.form.get("category")
+        content = request.form.get("content")
+        unlisted = request.form.get("unlisted") or False
+        try:
+            contentType = ContentType(request.form.get("contentType"))
+        except ValueError:
+            return Response(status=httpStatus.BAD_REQUEST)
+        if (
+            not (visibility := request.form.get("visibility").upper())
+            in post_visibility_map
+        ):
+            return Response(status=httpStatus.BAD_REQUEST)
+        private = post_visibility_map[visibility]
+    if request.method == "POST":
         post.author = author
         post.title = title
         post.category = category
@@ -149,14 +172,17 @@ def specific_post(author_id: int, post_id: int) -> Response:
         post.contentType = contentType
         post.private = private
         post.unlisted = unlisted
-        post.post_id = post_id
         db.session.commit()
         return make_response(jsonify(post.json())), httpStatus.OK
     elif request.method == "PUT":
         if Post.query.filter_by(id=post_id).first():
-            return Response(status=httpStatus.CONFLICT)
-        post = Post(author, title, category, content,
-                    contentType, private, unlisted, post_id)
+            return (
+                make_response(jsonify(error=res_msg.CREATE_CONFLICT)),
+                httpStatus.BAD_REQUEST,
+            )
+        post = Post(
+            author, title, category, content, contentType, private, unlisted, post_id
+        )
         db.session.add(post)
         db.session.commit()
         return Response(status=httpStatus.CREATED)
@@ -166,13 +192,15 @@ def specific_post(author_id: int, post_id: int) -> Response:
         return Response(status=httpStatus.NO_CONTENT)
 
 
-@bp.route('/login', methods=['POST'])
+@bp.route("/login", methods=["POST"])
 def login() -> Response:
     # get token from authorization header
-    token = request.form.get('token')
+    token = request.form.get("token")
 
     if not token:
-        return utils.json_response(httpStatus.UNAUTHORIZED, {"message": res_msg.TOKEN_MISSING})
+        return utils.json_response(
+            httpStatus.UNAUTHORIZED, {"message": res_msg.TOKEN_MISSING}
+        )
 
     try:
         author, decoded_token = utils.get_author(token)
@@ -182,22 +210,21 @@ def login() -> Response:
             utils.create_author(decoded_token)
             # login_user(author)
             return utils.json_response(
-                httpStatus.OK, 
-                { "message": res_msg.SUCCESS_USER_CREATED}
+                httpStatus.OK, {"message": res_msg.SUCCESS_USER_CREATED}
             )
         else:
             login_user(author)
             return utils.json_response(
-                httpStatus.OK, 
-                {"message": res_msg.SUCCESS_VERIFY_USER}
+                httpStatus.OK, {"message": res_msg.SUCCESS_VERIFY_USER}
             )
     except Exception as e:
         return utils.json_response(
-            httpStatus.INTERNAL_SERVER_ERROR, 
-            {"message": res_msg.GENERAL_ERROR + str(e)}
+            httpStatus.INTERNAL_SERVER_ERROR,
+            {"message": res_msg.GENERAL_ERROR + str(e)},
         )
 
-@bp.route('/login_test', methods=['GET'])
+
+@bp.route("/login_test", methods=["GET"])
 @login_required
 def login_test() -> Response:
-    return make_response(jsonify(message='Successful log in')), httpStatus.OK
+    return make_response(jsonify(message="Successful log in")), httpStatus.OK
