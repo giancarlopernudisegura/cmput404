fd8a54131f6c45031d46a8e10043f3d3cb9cefcf
Della <humanita.1999@gmail.com>
2022-02-22 20:04:01 -0700

    added buttons to post and tested db

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,8 @@
 import { h } from 'preact';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+
 
 /*
     Post component
@@ -13,10 +17,18 @@ type PostProps = {
 function Post({ title, body, author }: PostProps) {
 
     return (
-        <div className='bg-zinc-100 border-solid border-1 border-slate-500 w-2/3 m-auto rounded-lg'>
+        <div className='bg-zinc-100 border-solid border-1 border-slate-600 w-2/3 m-auto rounded-lg p-4'>
             <h1>{title}</h1>
             <p>{body}</p>
             <p>Author: {author}</p>
+            <div className="grid grid-cols-1 divide-y py-4">
+                <div className='flex flex-row gap-x-4'>
+                    <FavoriteBorderOutlinedIcon />
+                    <ChatBubbleOutlineOutlinedIcon />
+                    <ShareOutlinedIcon />
+                </div>
+            </div>
+
         </div>
     );
 
