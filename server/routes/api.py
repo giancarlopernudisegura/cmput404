eb9ebdc9f2f548c71d50c7ce130aa7d458cc01a3
giancarlopernudisegura <pernudig@gmail.com>
2022-02-22 23:58:09 -0700

    return empty followers list when no follower is found

--- server/routes/api.py
+++ server/routes/api.py
@@ -33,7 +33,8 @@ def pagination(
     Returns:
         A tuple of (page, size) to use for pagination."""
     size = int(arguments.get("size", str(default_page_size)), base=10)
-    page_number = int(arguments.get("page_number", str(default_page_number)), base=10)
+    page_number = int(arguments.get(
+        "page_number", str(default_page_number)), base=10)
     return page_number, size
 
 
@@ -125,14 +126,15 @@ def post(author_id: int) -> Response:
             return Response(status=httpStatus.BAD_REQUEST)
 
         if (
-            not (visibility := request.form.get("visibility").upper())
+            not (visibility:= request.form.get("visibility").upper())
             in post_visibility_map
         ):
             # bad visibility type or no visibility given
             return Response(status=httpStatus.BAD_REQUEST)
         private = post_visibility_map[visibility.upper()]
 
-        post = Post(author, title, category, content, contentType, private, unlisted)
+        post = Post(author, title, category, content,
+                    contentType, private, unlisted)
         db.session.add(post)
         db.session.commit()
         return Response(status=httpStatus.OK)
@@ -164,7 +166,7 @@ def specific_post(author_id: int, post_id: int) -> Response:
         except ValueError:
             return Response(status=httpStatus.BAD_REQUEST)
         if (
-            not (visibility := request.form.get("visibility").upper())
+            not (visibility:= request.form.get("visibility").upper())
             in post_visibility_map
         ):
             return Response(status=httpStatus.BAD_REQUEST)
@@ -201,7 +203,8 @@ def specific_post(author_id: int, post_id: int) -> Response:
 def get_comments(author_id: int, post_id: int) -> Response:
     page, size = pagination(request.args)
     comments = (
-        Comment.query.filter_by(post=post_id).paginate(page=page, per_page=size).items
+        Comment.query.filter_by(post=post_id).paginate(
+            page=page, per_page=size).items
     )
     return (
         make_response(
@@ -248,18 +251,19 @@ def post_comment(author_id: int, post_id: int) -> Response:
 def get_followers(author_id: int) -> Response:
     followers = Requests.query.filter_by(to=author_id).all()
     return (
-        make_response(jsonify(type="followers", items=[f.json() for f in followers])),
+        make_response(jsonify(type="followers", items=[
+                      f.json() for f in followers])),
         httpStatus.OK,
     )
 
 
 @bp.route("/authors/<int:author_id>/followers/<int:follower_id>", methods=["GET"])
 def is_follower(author_id: int, follower_id: int) -> Response:
-    follower = Requests.query.filter_by(to=author_id, initiated=follower_id).first()
-    if not follower:
-        return Response(status=httpStatus.NOT_FOUND)
+    follower = Requests.query.filter_by(
+        to=author_id, initiated=follower_id).first()
     return (
-        make_response(jsonify(type="followers", items=[follower.json()])),
+        make_response(jsonify(type="followers", items=(
+            [follower.json()] if follower else []))),
         httpStatus.OK,
     )
 
@@ -272,7 +276,8 @@ def remove_follower(author_id: int, follower_id: int) -> Response:
             make_response(jsonify(error=res_msg.NO_PERMISSION)),
             httpStatus.UNAUTHORIZED,
         )
-    follower = Requests.query.filter_by(to=author_id, initiated=follower_id).first()
+    follower = Requests.query.filter_by(
+        to=author_id, initiated=follower_id).first()
     if not follower:
         return Response(status=httpStatus.NOT_FOUND)
     db.session.delete(follower)
@@ -288,7 +293,8 @@ def add_follower(author_id: int, follower_id: int) -> Response:
             make_response(jsonify(error=res_msg.NO_PERMISSION)),
             httpStatus.FORBIDDEN,
         )
-    follower = Requests.query.filter_by(to=author_id, initiated=current_user.id).first()
+    follower = Requests.query.filter_by(
+        to=author_id, initiated=current_user.id).first()
     if follower:
         return (
             make_response(jsonify(error=res_msg.CREATE_CONFLICT)),
@@ -318,8 +324,8 @@ def login() -> Response:
             new_author = utils.create_author(decoded_token)
             login_user(author)
             return utils.json_response(
-                httpStatus.OK, 
-                { 
+                httpStatus.OK,
+                {
                     "message": res_msg.SUCCESS_USER_CREATED,
                     "data": new_author.json()
                 }
@@ -327,7 +333,7 @@ def login() -> Response:
         else:
             login_user(author)
             return utils.json_response(
-                httpStatus.OK, 
+                httpStatus.OK,
                 {
                     "message": res_msg.SUCCESS_VERIFY_USER,
                     "data": author.json()
@@ -338,6 +344,7 @@ def login() -> Response:
             httpStatus.INTERNAL_SERVER_ERROR,
             {"message": res_msg.GENERAL_ERROR + str(e)},
         )
+
 
 @bp.route('/logout')
 @login_required
@@ -354,6 +361,7 @@ def logout() -> Response:
             {"message": res_msg.LOGOUT_ERROR}
         )
 
+
 @bp.route('/user_me')
 @login_required
 def get_user_me() -> Response:
@@ -368,9 +376,10 @@ def get_user_me() -> Response:
         )
     except Exception as e:
         return utils.json_response(
-            httpStatus.INTERNAL_SERVER_ERROR, 
+            httpStatus.INTERNAL_SERVER_ERROR,
             {"message": res_msg.GENERAL_ERROR + str(e)}
         )
+
 
 @bp.route('/update_me', methods=['POST'])
 @login_required
@@ -387,9 +396,10 @@ def update_myself() -> Response:
         )
     except Exception as e:
         return utils.json_response(
-            httpStatus.INTERNAL_SERVER_ERROR, 
+            httpStatus.INTERNAL_SERVER_ERROR,
             {"message": res_msg.GENERAL_ERROR + str(e)}
         )
+
 
 @bp.route('/login_test', methods=['GET'])
 @login_required
