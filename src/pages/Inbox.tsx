e32bc89293872e6037a9921f16cf5f1051510c72
Lidia Ataupillco <ataupill@ualberta.ca>
2022-03-18 18:47:20 -0600

    cleaned up code

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -5,31 +5,12 @@ import { getPosts, get_author_id, inboxCall } from '../utils/apiCalls';
 
 type notiProps = { path: string };
 
+// TODO: finish implementation
 function Inbox({ path }: notiProps) {
   const [ posts, setPosts ] = useState();
   const [ likes, setLikes ] = useState();
   const [ follow, setFollow ] = useState();
   const [ errMsg, setErrMsg ] = useState("");
-
-  useEffect(() => {
-    function getPostsFromAPI() {
-      console.log("Getting posts from API...");
-      get_author_id()
-          .then(author_id => {
-              const response = inboxCall(author_id, "GET");
-              response
-                  .then(data => {
-                      // TODO: temp, only use posts from inbox for now
-                      setPosts(data.items.filter((item: any) => item.type == "post"));
-                  })
-                  .catch(err => {
-                      setErrMsg(err.message);
-                  });
-          })
-          .catch(console.error);
-  }
-  getPostsFromAPI();
-  }, []);
 
   return (
     <div>
