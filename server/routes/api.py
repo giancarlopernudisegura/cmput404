789644a83affc65422060050fd0e642df4816aff
Pegmode <dchu@ualberta.ca>
2022-03-24 01:52:54 +0000

    changed all login_required wrappers in api to requre_auth

--- server/routes/api.py
+++ server/routes/api.py
@@ -176,7 +176,7 @@ def post(author_id: str) -> Response:
     "/authors/<string:author_id>/posts/<string:post_id>",
     methods=["POST", "PUT", "DELETE"],
 )
-@login_required
+@require_authentication
 def specific_post(author_id: str, post_id: str) -> Response:
     print(f"AUTHOR_ID {author_id}\nTYPE:{type(author_id)}")
     """Modify, create or delete a specific post."""
@@ -271,7 +271,7 @@ def get_comment(author_id: str, post_id: str, comment_id: str) -> Response:
 @bp.route(
     "/authors/<string:author_id>/posts/<string:post_id>/comments", methods=["POST"]
 )
-@login_required
+@require_authentication
 def post_comment(author_id: str, post_id: str) -> Response:
     try:
         title = request.json["title"]
@@ -336,7 +336,7 @@ def is_follower(author_id: str, follower_id: str) -> Response:
 @bp.route(
     "/authors/<string:author_id>/followers/<string:follower_id>", methods=["DELETE"]
 )
-@login_required
+@require_authentication
 def remove_follower(author_id: str, follower_id: str) -> Response:
     if current_user.id != follower_id:
         return (
@@ -360,7 +360,7 @@ def remove_follower(author_id: str, follower_id: str) -> Response:
 
 
 @bp.route("/authors/<string:author_id>/followers/<string:follower_id>", methods=["PUT"])
-@login_required
+@require_authentication
 def add_follower(author_id: str, follower_id: str) -> Response:
     if current_user.id != follower_id:
         return (
@@ -382,7 +382,7 @@ def add_follower(author_id: str, follower_id: str) -> Response:
 
 
 @bp.route("/authors/<string:author_id>/inbox", methods=["GET"])
-@login_required
+@require_authentication
 def get_inbox(author_id: str) -> Response:
     if current_user.id != author_id:
         return (
@@ -435,7 +435,7 @@ def post_inbox(author_id: str) -> Response:
 
 
 @bp.route("/authors/<string:author_id>/inbox", methods=["DELETE"])
-@login_required
+@require_authentication
 def clear_inbox(author_id: str) -> Response:
     if current_user.id != author_id:
         return (
@@ -667,7 +667,7 @@ def login_unit_test() -> Response:
 
 
 @bp.route("/logout", methods=["POST"])
-@login_required
+@require_authentication
 def logout() -> Response:
     try:
         logout_user()
@@ -679,13 +679,13 @@ def logout() -> Response:
 
 
 @bp.route("/login_test", methods=["GET"])
-@login_required
+@require_authentication
 def login_test() -> Response:
     return make_response(jsonify(message="Successful log in")), httpStatus.OK
 
 
 @bp.route("/user_me")
-@login_required
+@require_authentication
 def get_user_me() -> Response:
     try:
         return utils.json_response(
@@ -700,7 +700,7 @@ def get_user_me() -> Response:
 
 
 @bp.route("/admin/author/<string:author_id>", methods=["POST"])
-@login_required
+@require_authentication
 def modify_author(author_id: str) -> Response:
     if not current_user.isAdmin:
         return (
@@ -723,7 +723,7 @@ def modify_author(author_id: str) -> Response:
 
 
 @bp.route("/admin/author", methods=["PUT"])
-@login_required
+@require_authentication
 def create_author() -> Response:
     if not current_user.isAdmin:
         return (
@@ -763,7 +763,7 @@ def create_author() -> Response:
 
 
 @bp.route("/admin/author/<string:author_id>", methods=["DELETE"])
-@login_required
+@require_authentication
 def delete_author(author_id: str) -> Response:
     if not current_user.isAdmin:
         return (
@@ -785,7 +785,7 @@ def delete_author(author_id: str) -> Response:
 
 
 @bp.route("/admin/settings", methods=["GET"])
-@login_required
+@require_authentication
 def get_settings() -> Response:
     if not current_user.isAdmin:
         return (
@@ -805,7 +805,7 @@ def get_settings() -> Response:
 
 
 @bp.route("/admin/settings", methods=["POST"])
-@login_required
+@require_authentication
 def set_settings() -> Response:
     if not current_user.isAdmin:
         return (
