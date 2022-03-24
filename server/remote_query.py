2b0e20bd4f0a1334a8ec368befac9e3bd80a76ee
Pegmode <dchu@ualberta.ca>
2022-03-24 18:51:08 +0000

    changed remote host fetching to db

--- server/remote_query.py
+++ server/remote_query.py
@@ -1,76 +1,73 @@
 from server.models import Author, Inbox, Post, Comment, Requests, Like, Remote_Node
-from server.utils.remote_constants import *
 import requests
 
     
 def get_all_remote_authors():#for downed remote https://website404.herokuapp.com/authors?size=10&page=1
+    nodes = Remote_Node.query.all()
     items = []   
-    remote_node_1 = Remote_Node.query.filter_by(id=REMOTE_NODE1_HOST).first()
-    r = requests.get(f"{remote_node_1.id}authors")
-    items.extend(r.json()["items"])
-
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors?size=30&page=1")#node2 enforces paging in all cases
-    items.extend(r.json()["items"])
+    for node in nodes:
+        r = requests.get(f"{node.id}authors?size=30&page=1")
+        if r.status_code == 200: 
+            items.extend(r.json()["items"])
     return items
 
 def get_remote_author(author_id: str):
-    #Non functional remote endpoint?
-    # remote_node_1 = Remote_Node.query.filter_by(id=REMOTE_NODE1_HOST).first()
-    # r = requests.get(f"{remote_node_1.id}authors/{author_id}", auth=(remote_node_1.user, remote_node_1.password))
-    # try:
-        
-
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}")
-    try:
-        r.json()["type"]
-    except KeyError:
-        return None#author not found on server
-    if r.json()["type"] == "author":
-        return r.json()
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}")
+        if r.status_code == 200 and r.json()["type"] == "author":
+            return r.json() 
+    return None#Author not found in any remotes
 
 def get_remote_author_posts(author_id: str):
     items = []
-    # remote_node_1 = Remote_Node.query.filter_by(id=REMOTE_NODE1_HOST).first()
-    # r = requests.get(f"{remote_node_1.id}authors/{author_id}/posts/{post_id}", auth=(remote_node_1.user, remote_node_1.password))
-    # items.extend(r.json()["items"])
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}/posts?size=30&page=1")
+        if r.status_code == 200 and r.json()["type"] == "author":
+            items.extend(r.json()["items"])
+    return items
 
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}/posts?size=30&page=1")
-    items.extend(r.json()["items"])
-    return r.json()["items"]
 
 #below untested
 
-def get_remote_post(author_id: str, post_id: str):
+def get_remote_post(author_id: str, post_id: str):#tested for https://backend-404.herokuapp.com
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}")
+        if r.status_code == 200 and r.json()["type"] == "post":
+            return r.json() 
+    return None#post not found
 
-    #auth?
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}/posts/{post_id}")
-    return r.json()
 
-def get_remote_comments(author_id: str, post_id: str):
-    items = []
+def get_remote_comments(author_id: str, post_id: str):#get all posts comments
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}/comments?size=30&page=1")
+        if r.status_code == 200 and r.json()["type"] == "comments":
+            return r.json()["items"]
+    return []
 
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}/posts/{post_id}/comments?size=30&page=1")
-    items.extend(r.json()["items"])
-    return r.json()["items"]
-
-def get_remote_comments(author_id: str, post_id: str, comment_id: str):
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}/posts/{post_id}/comments/{comment_id}")
-    return r.json()["items"]
+def get_remote_comment(author_id: str, post_id: str, comment_id: str):#get single comment
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}/comments/{comment_id}")
+        if r.status_code == 200 and r.json()["type"] == "comment":
+            return r.json()
+    return None
 
 def get_remote_post_likes(author_id: str, post_id: str):    
-
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}/posts/{post_id}/likes?size=30&page=1")
-    return r.json()["items"]
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}/likes?size=30&page=1")
+        if r.status_code == 200 and r.json()["type"] == "likes":
+            return r.json()["items"]
+    return []
 
 def get_remote_comment_likes(author_id: str, post_id: str, comment_id: str):
-
-    remote_node_2 = Remote_Node.query.filter_by(id=REMOTE_NODE2_HOST).first()
-    r = requests.get(f"{remote_node_2.id}authors/{author_id}/posts/{post_id}/comments/{comment_id}/likes?size=30&page=1")
-    return r.json()["items"]
+    nodes = Remote_Node.query.all()
+    for node in nodes:
+        r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}/comments/{comment_id}/likes?size=30&page=1")
+        if r.status_code == 200 and r.json()["type"] == "likes":
+            return r.json()["items"]
+    return []
--- server/utils/remote_constants.py
+++
@@ -1,2 +0,0 @@
-REMOTE_NODE1_HOST = "https://deployservertest.herokuapp.com/"
-REMOTE_NODE2_HOST = "https://website404.herokuapp.com/"
