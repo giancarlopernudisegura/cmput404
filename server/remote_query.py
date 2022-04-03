1764f0025621938f54e663fc6e28d8a7f8f4f2fe
Pegmode <dchu@ualberta.ca>
2022-04-03 21:37:37 +0000

    added node 3 like formatting

--- server/remote_query.py
+++ server/remote_query.py
@@ -182,16 +182,24 @@ def get_remote_post_likes(author_id: str, post_id: str):
     nodes = Remote_Node.query.all()
     for node in nodes:
         r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}/likes?size=30&page=1", auth=(node.user, node.password))
-        if r.status_code == 200 and (r.json()["type"] == "likes" or r.json()["type"] == "liked"):
-            return {"likes":r.json()["items"]}
+        if "type" in r.json():#nodes 1 and 2
+            if r.status_code == 200 and (r.json()["type"] == "likes" or r.json()["type"] == "liked"):
+                return {"likes":r.json()["items"]}
+        else:
+            if r.status_code == 200:
+                return {"likes":r.json()}
     return []
 
 def get_remote_comment_likes(author_id: str, post_id: str, comment_id: str):
     nodes = Remote_Node.query.all()
     for node in nodes:
         r = requests.get(f"{node.id}authors/{author_id}/posts/{post_id}/comments/{comment_id}/likes?size=30&page=1", auth=(node.user, node.password))
-        if r.status_code == 200 and (r.json()["type"] == "likes" or r.json()["type"] == "liked"):
-            return {"likes":r.json()["items"]}
+        if "type" in r.json():#nodes 1 and 2
+            if r.status_code == 200 and (r.json()["type"] == "likes" or r.json()["type"] == "liked"):
+                return {"likes":r.json()["items"]}
+        else:
+            if r.status_code == 200:
+                return {"likes":r.json()}
     return []
 
 
