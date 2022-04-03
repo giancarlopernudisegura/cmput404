a7c0b04a3662027b698087e8a76f61c21670d0c9
Lidia Ataupillco <ataupill@ualberta.ca>
2022-04-03 15:40:19 -0600

    added pagination

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 import { useState, useEffect } from "preact/hooks";
 import DrawerMenu from "../components/sidemenu-components/Drawer";
-import { Alert, CircularProgress } from "@mui/material";
+import { Alert, Button, CircularProgress } from "@mui/material";
 
 import {
   getPosts,
@@ -16,9 +16,8 @@ import {
 import PostList from "../components/PostList";
 import AuthorInfo from "../components/profile/AuthorInfo";
 import DialogTemplate from '../components/DialogTemplate';
-import { MARKDOWN } from '../utils/constants';
+import { LOAD_MORE_TEXT, MARKDOWN, NO_MORE_POSTS_TEXT } from '../utils/constants';
 import ShareList from "../components/ShareList";
-
 
 type profileProps = { path: string };
 
@@ -39,21 +38,45 @@ function Profile({ path }: profileProps) {
   // get author data 
   const [ author, setAuthor ] = useState(Object());
   const [ myPosts, setMyPosts ] = useState(Array());
+  const [ postPage, setPostPage ] = useState(1);
+  const [ buttonText, setButtonText ] = useState(LOAD_MORE_TEXT);
   const BACKEND_HOST = process.env.FLASK_HOST;
 
   //Posts that have been shared by author
   const [sharedPosts, setSharedPosts] = useState(Array());
 
+  const getNextPostPage = async () => {
+    try {
+        const postsRes = await getPosts(author.id, postPage);
+        const fetchedPosts = postsRes.items;
+        if (fetchedPosts.length === 0) {
+            alert("There are no more posts to show");
+            setButtonText(NO_MORE_POSTS_TEXT);
+            return;
+        }
+        setMyPosts([...myPosts, ...fetchedPosts]);
+        // update post page
+        setPostPage(postPage + 1);
+    } catch (err) {
+        setErrMsg((err as Error).message);
+    }
+  }
+
   useEffect(() => {
     const authorPromise = get_author_id().then((author_id : any) => {
       getSpecAuthor(author_id).then(data => {
         setAuthor(data);
+        console.log("DATA", data);
       });
       return author_id;
     });
 
     // Set the author's posts
-    var postsPromise = authorPromise.then(authorId => { return getPosts(authorId); });
+    var postsPromise = authorPromise.then(authorId => { 
+      let posts = getPosts(authorId, postPage); 
+      setPostPage(postPage + 1);
+      return posts;
+    });
     postsPromise.then(posts => {  setMyPosts(posts.items); });
 
     Promise.all([authorPromise, postsPromise])
@@ -164,6 +187,14 @@ function Profile({ path }: profileProps) {
                 onShare={sharePost}
                 handleEdit={handleEdit}
               />
+
+              <Button
+                className="w-fit-center"
+                variant="contained"
+                onClick={() => getNextPostPage()}
+              >
+                {buttonText}
+              </Button>
 
               {openDialog && 
                 <DialogTemplate 
