849acb7fb97373942f48dea5e3007f01de48b9ad
Della <humanita.1999@gmail.com>
2022-04-03 17:46:59 -0600

    used custom hooks in AuthorInfo

--- src/components/profile/AuthorInfo.tsx
+++ src/components/profile/AuthorInfo.tsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'preact/hooks';
 import { getFollowers, followerCall } from '../../utils/apiCalls';
 import EditIcon from "@mui/icons-material/Edit";
 
+import useFriends from '../hooks/useFriends';
+import useFollowers from '../hooks/useFollowers';
+
 
 type AuthorProps = {
     author: any,
@@ -10,56 +13,16 @@ type AuthorProps = {
 };
 
 function AuthorInfo({ author, is_owner = false }: AuthorProps) {
-    const [myFollowers, setMyFollowers] = useState(Array());
-    const [myFriends, setMyFriends] = useState(Array())
+    // const [myFollowers, setMyFollowers] = useState(Array());
+    // const [myFriends, setMyFriends] = useState(Array())
 
     if (author === undefined) {
         return <h1>Error loading information about this author.</h1>;
     }
 
-    useEffect(() => {
+    const myFollowers = useFollowers(author.id);
+    const myFriends = useFriends(author.id, myFollowers);
 
-        async function fetchFollowers() {
-            try {
-                let result = await getFollowers(author.id);
-                let followers = result.items;
-                setMyFollowers(followers);
-
-                return followers;
-            }
-            catch (err) {
-                console.log('Error fetching followers:', err);
-            }
-        }
-
-        async function fetchFriends() {
-
-            try {
-                let followers = await fetchFollowers();
-                var friends = new Array();
-
-                for (let follower of followers) {
-                    let res = await followerCall(follower.id, author.id, "GET");
-                    // Make an API call to check if the author is following the follower
-                    if (res.status === 200) {
-                        let data = res.items;
-                        // if the array is non-empty, it is a follow
-                        if (data.length > 0) {
-                            friends.push(follower);
-                        }
-                    } else {
-                        throw Error();
-                    }
-                }
-                setMyFriends(friends);
-
-            } catch (err) {
-                console.log('Error fetching friends:', err);
-            }
-
-        }
-        fetchFriends();
-    }, []);
 
     const editName = async () => {
         const name = window.prompt('Edit name');
@@ -94,9 +57,8 @@ function AuthorInfo({ author, is_owner = false }: AuthorProps) {
                         className="text-sm font-thin text-gray-400 hover:text-blue-500">
                         {author.github}
                     </a>
-                    : null}
+                : null}
 
-                {/* TODO: add friends */}
                 <div className='flex flex-row space-x-8'>
                     <span>{myFriends.length} Friends</span>
                     <span>{myFollowers.length} Followers</span>
